9b9ec0ff32092ae2fed0e51f3c5e8c9646cee74d
jemchicapop <efloresaguilar@gmail.com>
2019-06-20 14:16:05 +0200

    modificacion 2
    Signed-off-by: jemchicapop <efloresaguilar@gmail.com>

--- pruebaIcoMobile/www/js/maps.js
+++ pruebaIcoMobile/www/js/maps.js
@@ -9,8 +9,8 @@ var app = {
   },
 
   dispositivoListo: function(){
-    //navigator.geolocation.getCurrentPosition(app.pintaCoordenadasEnMapa, app.errorAlSolicitarLocalizacion);
-    navigator.geolocation.getCurrentPosition(app.dibujaCoordenadas, app.errorAlSolicitarLocalizacion);
+    navigator.geolocation.getCurrentPosition(app.pintaCoordenadasEnMapa, app.errorAlSolicitarLocalizacion);
+    //navigator.geolocation.getCurrentPosition(app.dibujaCoordenadas, app.errorAlSolicitarLocalizacion);
   },
 
   dibujaCoordenadas: function(position){
@@ -18,7 +18,7 @@ var app = {
   	coordsDiv.innerHTML = 'Latitud: ' + position.coords.latitude + ' Longitud: ' + position.coords.longitude;
   },
 
-  /*pintaCoordenadasEnMapa: function(position){
+  pintaCoordenadasEnMapa: function(position){
     var miMapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ29vdHlmZXIiLCJhIjoiY2l1MGlrb2M3MDAwMDJ6bXAxY3dlOXdkYiJ9.RBfUsuzHfLrofEyMR8IVlA', {
@@ -37,7 +37,7 @@ var app = {
   pintaMarcador: function(latlng, texto, mapa){
     var marcador = L.marker(latlng).addTo(mapa);
     marcador.bindPopup(texto).openPopup();
-  },*/
+  },
 
   errorAlSolicitarLocalizacion: function(error){
     console.log(error.code + ': ' + error.message);
